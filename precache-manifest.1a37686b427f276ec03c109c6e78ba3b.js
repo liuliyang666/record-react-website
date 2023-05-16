102b9329fc59b6b1d5aa714b755c37bf79bb7a7b
lilyyang <1592218619@qq.com>
2023-05-16 12:01:07 +0800

    deploy

---
+++ precache-manifest.1a37686b427f276ec03c109c6e78ba3b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f3e2ba9f8de755770f4736a695f927d",
+    "url": "/record-react-website/index.html"
+  },
+  {
+    "revision": "89c2e0c3f1174e721def",
+    "url": "/record-react-website/static/css/main.573c9078.chunk.css"
+  },
+  {
+    "revision": "9a40f5909432249abf80",
+    "url": "/record-react-website/static/js/2.37b024eb.chunk.js"
+  },
+  {
+    "revision": "99406820fc9e02f862dbd66e126ca114",
+    "url": "/record-react-website/static/js/2.37b024eb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "89c2e0c3f1174e721def",
+    "url": "/record-react-website/static/js/main.9b1baffa.chunk.js"
+  },
+  {
+    "revision": "d64c47068585c19b3483",
+    "url": "/record-react-website/static/js/runtime-main.ac4719cc.js"
+  }
+]);
